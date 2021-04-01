d7e1a6d294176ab7ce5c9dadf19e440b46b1a6a2
sarahsau <sausan.sarah@gmail.com>
2021-04-01 15:04:16 +0700

    delete migration files

--- db/migrate/202103251454_converters_drop_result.rb
+++
@@ -1,5 +0,0 @@
-class ConvertersDropResult < ActiveRecord::Migration[6.1]
-  def self.up
-    remove_column :converters, :result
-  end
-end
--- db/migrate/202103251819_rename_converters_columns.rb
+++
@@ -1,7 +0,0 @@
-class RenameConvertersColumns < ActiveRecord::Migration[6.1]
-  def self.up
-    rename_column :converters, :banks, :bank
-    rename_column :converters, :files, :statement
-    rename_column :converters, :results, :result
-  end
-end
